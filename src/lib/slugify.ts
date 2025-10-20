288a8d8d93dd8fcda38716cd8dd441bcaabd6387
John Aerial J. Azcune <65322242+STICKnoLOGIC@users.noreply.github.com>
2025-10-20 10:58:36 +0800

    Remove lowercase conversion in slugify function

--- src/lib/slugify.ts
+++ src/lib/slugify.ts
@@ -1,3 +1,3 @@
 export function slugify(input: string): string {
-    return input.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)/g, "");
+    return input.replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)/g, "");
 }
